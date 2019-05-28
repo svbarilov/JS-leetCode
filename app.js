e7d36cd84442d8c1896f4419bed5548366b7b283
Sergei <sbarylau@salesforce.com>
2019-05-27 17:21:55 -0700

    Adds twoSum solution

---
+++ app.js
@@ -0,0 +1,1 @@
+// executable JS goes here
---
+++ leetCode/easy/twoSum.js
@@ -0,0 +1,26 @@
+
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+//
+//     You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//
+//     Example:
+//
+// Given nums = [2, 7, 11, 15], target = 9,
+//
+//     Because nums[0] + nums[1] = 2 + 7 = 9,
+// return [0, 1].
+
+
+var target = 6;
+var nums = [3,2,4];
+var twoSum = function(nums, target) {
+    map = {};
+    for (i = 0; i < nums.length; i++) {
+        if (map[target - nums[i]] != undefined) {
+            return [map[target - nums[i]], i]
+        }
+        map[nums[i]] = i;
+    }
+};
+
+twoSum(nums, target);
